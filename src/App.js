8f277cd023baf8cd3a7da0127de06949a049e5c6
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2022-10-29 11:50:03 +0530

    adde components and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import About from './components/About'
 export default function App() {
   return (
-    <div>This is iNotebook</div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />}   />
+        <Route exact path='/about' element={<About />}   />
+      </Routes>
+      
+    </Router>
   )
 }
