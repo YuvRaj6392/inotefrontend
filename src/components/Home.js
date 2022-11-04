00079eb751593a7b1c9ceb9973a1ced18987d4cf
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2022-11-04 23:56:25 +0530

    added changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,37 @@
 import React from 'react'
-
+import { useEffect } from 'react';
+import { useContext } from 'react'
+import noteContext from '../context/notes/NotesContext';
+import NoteState from '../context/notes/NotesStates';
 export default function Home() {
+  const context=useContext(noteContext);
+  console.log(context.notes)
+ useEffect(()=>{
+  setTimeout(() => {
+      context.setNotes( [
+        {
+          "_id": "636555c8f181f0e226c66992",
+          "user": "6365555af181f0e226c6698f",
+          "title": "tuesdat Notes",
+          "description": "It was a nice day today",
+          "tag": "General",
+          "createdAt": "2022-11-04T18:11:20.583Z",
+          "updatedAt": "2022-11-04T18:11:20.583Z",
+          "__v": 0
+        },
+        {
+          "_id": "636555d9f181f0e226c66994",
+          "user": "6365555af181f0e226c6698f",
+          "title": "saturday Notes",
+          "description": "It was a nice day on tuesday",
+          "tag": "General",
+          "createdAt": "2022-11-04T18:11:37.068Z",
+          "updatedAt": "2022-11-04T18:11:37.068Z",
+          "__v": 0
+        }
+      ])
+  }, 5000);
+ },[])
   return (
     <div >
     <div className="container m-3">
@@ -23,8 +54,13 @@ export default function Home() {
 </form>
   <h1>Your notes</h1>
     </div>
-    
-  
+{
+  context.notes.map((element)=>{
+    return <div>
+      {element.title}
+    </div>
+  })
+}
     </div>
   )
 }
--- src/context/notes/NotesStates.js
+++ src/context/notes/NotesStates.js
@@ -3,24 +3,32 @@
 import { useState } from 'react'
 import noteContext from './NotesContext'
 const NoteState=(props)=>{
-    // const s1={
-    //     "name":"yuvraj",
-    //     "occupation":"software engineer"
-    // }
-
-    // const update=()=>{
-    //     setTimeout(() => {
-    //         setState({
-    //             "name":"harry",
-    //             "occupation":"Front end developer"
-    //         }) 
-    //     }, 2000);
-    // }
-
-    // const [state,setState]=useState(s1);
-
+  const notesInitial=
+     [
+      {
+        "_id": "636555c8f181f0e226c66992",
+        "user": "6365555af181f0e226c6698f",
+        "title": "Monday Notes",
+        "description": "It was a nice day today",
+        "tag": "General",
+        "createdAt": "2022-11-04T18:11:20.583Z",
+        "updatedAt": "2022-11-04T18:11:20.583Z",
+        "__v": 0
+      },
+      {
+        "_id": "636555d9f181f0e226c66994",
+        "user": "6365555af181f0e226c6698f",
+        "title": "tuesday Notes",
+        "description": "It was a nice day on tuesday",
+        "tag": "General",
+        "createdAt": "2022-11-04T18:11:37.068Z",
+        "updatedAt": "2022-11-04T18:11:37.068Z",
+        "__v": 0
+      }
+    ]
+  const [notes,setNotes]=useState(notesInitial)
     return (
-<noteContext.Provider value={{}} >
+<noteContext.Provider value={{notes,setNotes}} >
         {props.children}
     </noteContext.Provider>
     )
