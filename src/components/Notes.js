3b44599f68682e810d80af94f91c9a9229cccaac
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2022-11-18 00:09:25 +0530

    edited the edit functionality

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -6,7 +6,11 @@ import AddNote from "./AddNote";
 import NoteItem from "./NoteItem";
 export default function Notes() {
    
-   const [note,setNote]=useState({etitle:"",edescription:"",etag:""})
+    const [etitle,seteTitle]=useState("");
+    const [edescription,seteDescription]=useState("");
+    const [etag,seteTag]=useState("General");
+   
+   
   const ref=useRef(null)
   const context = useContext(noteContext);
   const { notes, getNotes,editNote } = context;
@@ -14,26 +18,23 @@ export default function Notes() {
   useEffect(() => {
     console.log("hello!");
     getNotes();
-    //eslint-disable-next
   }, []);
   
 const [noteId,setNoteId]=useState('');
 
-  const updateNote = (currentNote) => {
+  const updateNote = (note) => {
     ref.current.click();
-    setNoteId(currentNote._id)
-    setNote({etitle:currentNote.title,edescription:currentNote.description,etag:currentNote.tag})
+    setNoteId(note._id);
+    seteTitle(note.title);
+    seteDescription(note.description);
+    seteTag(note.tag);
+
     
     
   };
 
   const updateModal=()=>{
-    editNote(noteId,note.etitle,note.edescription,note.etag)
-  }
-
-
-  const onChange=(e)=>{
-    setNote({...note,[e.target.name]:e.target.value})
+    editNote(noteId,etitle,edescription,etag)
   }
 
   
@@ -41,38 +42,44 @@ const [noteId,setNoteId]=useState('');
     <>
       <AddNote />
       
-<button ref={ref} type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style={{display:'none'}}>
+<button ref={ref} type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style={{display:'none'}}>
   Launch demo modal 
 </button>
 
 
-<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div className="modal-body">
+      <div class="modal-body">
       <form className='mb-3'>
   <div className="mb-3">
     <label  className="form-label">Title</label>
-    <input type="text" className="form-control"  name="etitle" value={note.etitle}     onChange={onChange}/>
+    <input type="text" className="form-control" id="etitle" name="etitle" value={etitle}      onChange={(e)=>{
+      seteTitle(e.target.value)
+    }}/>
    
   </div>
   <div className="mb-3">
     <label className="form-label">Description</label>
-    <input type="text" className="form-control" id="eDescription"  name="edescription" value={note.edescription}     onChange={onChange}/>
+    <input type="text" className="form-control" id="edescription" name="edescription" onChange={(e)=>{
+      seteDescription(e.target.value)
+    }}/>
   </div>
   <div className="mb-3">
     <label  className="form-label">Tag</label>
-    <input type="text" className="form-control" id="etag"  name="etag"   value={note.etag}   onChange={onChange}/>
+    <input type="text" className="form-control" id="etag" name="etag"  onChange={(e)=>{
+      seteTag(e.target.value)
+    }}/>
   </div>
   </form>
       </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary" onClick={updateModal}>update note</button>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" onClick={updateModal}>update note</button>
       </div>
     </div>
   </div>
